850899f0c0d5fa69eac9a3c35d80f0bb08c7c5f4
Lilney205 <lilney205@gmail.com>
2025-03-07 13:08:43 +0300

    Create Header.js

---
+++ Src/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white py-4 text-center text-xl font-bold">
+      My React App
+    </header>
+  );
+};
+
+export default Header;
